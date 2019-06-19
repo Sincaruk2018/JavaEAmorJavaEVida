211486a5bbbc6df437c4f4e006432cf37d9909a5
Luiz Vieira <40367461+Sincaruk2018@users.noreply.github.com>
2019-06-19 08:11:34 -0300

    Add files via upload

---
+++ Agendando.java
@@ -0,0 +1,60 @@
+package com.example.docaodesangue.activity;
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import com.example.docaodesangue.R;
+
+public class Agendando extends Activity implements Button.OnClickListener {
+
+    private Button botao;
+
+    static final int DATE_DIALOG_ID = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agendando2);
+        botao = (Button) findViewById(R.id.btn);
+        botao.setOnClickListener(this);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Calendar calendario = Calendar.getInstance();
+
+        int ano = calendario.get(Calendar.YEAR);
+        int mes = calendario.get(Calendar.MONTH);
+        int dia = calendario.get(Calendar.DAY_OF_MONTH);
+
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this, mDateSetListener, ano, mes,
+                        dia);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                      int dayOfMonth) {
+                    String data = String.valueOf(dayOfMonth) + " /"
+                            + String.valueOf(monthOfYear+1) + " /" + String.valueOf(year);
+
+                }
+            };
+
+    @Override
+    public void onClick(View v) {
+        if (v == botao)
+            showDialog(DATE_DIALOG_ID);
+    }
+}
